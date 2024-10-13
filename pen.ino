d2e5758b86b78b6bb3b2103480efa4588a28d7e4
Eric L <eliu15@uci.edu>
2024-10-13 12:40:04 -0700

    Small program to toggle LED blink on the ESP32 through bluetooth

--- pen.ino
+++ pen.ino
@@ -1,8 +1,91 @@
+#include <BLEDevice.h>
+#include <BLEService.h>
+#include <BLEUtils.h>
+
+constexpr const char SERVICE_UUID[] = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+constexpr const char CHARACTERISTIC_UUID[] =
+    "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+
+constexpr int BUILTIN_LED = 2;
+
+bool blink = true;
+bool paired = false;
+bool wasPaired = false;
+
+BLEServer *server = nullptr;
+
+class ServerNotifs : public BLEServerCallbacks {
+  void onConnect(BLEServer *) { paired = true; };
+
+  void onDisconnect(BLEServer *) { paired = false; }
+};
+
+class BlinkUpdate : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *c) {
+    auto bytes = c->getValue();
+    if (bytes.length() == 1) {
+      bool newBlink = static_cast<bool>(bytes[0]);
+      if (newBlink != blink) {
+        Serial.println("Toggling blink");
+      } else {
+        Serial.println("Blink set to the same value as before");
+      }
+      blink = newBlink;
+    } else {
+      Serial.println("Couldn't parse written value, ignored");
+    }
+  }
+};
+
 void setup() {
-    Serial.begin(115200);
+  // Configure serial and builtin LED
+  Serial.begin(115200);
+  pinMode(BUILTIN_LED, OUTPUT);
+
+  // Configure bluetooth
+  BLEDevice::init("ZotPen");
+
+  server = BLEDevice::createServer();
+  server->setCallbacks(new ServerNotifs());
+
+  auto *service = server->createService(SERVICE_UUID);
+  // Configure blink characteristic
+  auto *setBlink = service->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+
+  setBlink->setValue("\0");
+  setBlink->setCallbacks(new BlinkUpdate());
+
+  service->start();
+
+  auto *adv = server->getAdvertising();
+  adv->addServiceUUID(SERVICE_UUID);
+  adv->setScanResponse(true);
+  // functions that help with iPhone connections issue
+  adv->setMinPreferred(0x06);
+  adv->setMinPreferred(0x12);
+  adv->start();
 }
 
 void loop() {
-    Serial.println("Hello world!\n");
+  if (blink) {
+    digitalWrite(BUILTIN_LED, HIGH);
     delay(500);
+    digitalWrite(BUILTIN_LED, LOW);
+    delay(500);
+  } else {
+    delay(100);
+  }
+  // Connecting to device
+  if (paired && !wasPaired) {
+    wasPaired = true;
+  }
+  // Disconnecting to device
+  if (!paired && wasPaired) {
+    wasPaired = false;
+    delay(500);
+    server->startAdvertising();
+    Serial.println("Device unpaired, advertising again");
+  }
 }
