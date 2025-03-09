325373b091d3b1692e6625a92b7ccda2dc1b0f28
Kevin Thai <70713323+kevinthai-kbd@users.noreply.github.com>
2025-03-09 16:46:46 -0700

    added ocr files

---
+++ ocr/ocr.py
@@ -0,0 +1,39 @@
+#ocr imports
+from pix2text import Pix2Text
+
+#webbrowser
+import webbrowser
+import os
+
+def plot_equation(equation):
+    f = open('./output/desmos.html', 'w') 
+    html_template = f""" 
+    <script src="https://www.desmos.com/api/v1.10/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"></script>
+    <div id="calculator" style="width: 600px; height: 400px;"></div>
+        <script>
+            var elt = document.getElementById('calculator');
+            var calculator = Desmos.GraphingCalculator(elt);
+            calculator.setExpression({{ id: 'line1', latex: '{equation}', color: '#ff0000' }});
+        </script>
+    """
+    # writing the code into the file 
+    f.write(html_template) 
+  
+    # close the file 
+    f.close() 
+  
+    # 1st method how to open html files in chrome using 
+    filename = 'file:///'+os.getcwd()+'/output/' + 'desmos.html'
+    webbrowser.open_new_tab(filename) 
+
+def get_equation(path):
+    p2t = Pix2Text.from_config()
+    equation = p2t.recognize_formula(path)
+    print(equation)
+    return equation
+
+def main():
+    equation = get_equation("./input/yx.jpg")
+    plot_equation(equation)
+
+main()
